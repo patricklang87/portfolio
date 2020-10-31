1794200b135ece2813abc2b672bb6091144d226f
Patrick <patricklang87@gmail.com>
2020-10-31 12:11:59 -0400

    Fix linking issue

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ pushNewInfo("Sentence Assesser", "tbd", "An adaptation of Levenstein Distance, m
 
 pushNewInfo("Vocab List Bulider", "tbd", "A simple program that allows vocab words to be added to a database and then tested. Written in JavaScript to practice JSON, node.js, and database construction.", "Sept 2020", "Sept 2020")
 
-pushNewInfo("Battle Engine", "tbd", "A simple RPG battle engine designed to practice the use of objects in JavaScript. Features Beverly, Battle Bot, and a Pack of Wild Dogs.", "Sept 2020", "Sept 2020");
+pushNewInfo("Battle Engine", "https://patricklang87.github.io/battle_engine/", "A simple RPG battle engine designed to practice the use of objects in JavaScript. Features Beverly, Battle Bot, and a Pack of Wild Dogs.", "Sept 2020", "Sept 2020");
 
 pushNewInfo("Are you a Dweeb?", "tbd", "A quiz to determine whether you are a dweeb. Written quickly to practice radio buttons in the HTML DOM.", "Sept 2020", "Sept 2020");
 
@@ -28,12 +28,14 @@ pushNewInfo("The Coveted Object", "tbd", "A MidLibs style game to practice using
 let gridDivs = "";
 
 for (let i = 0; i < projArray.length; i++) {
-	let gridDiv = `<div id="${projArray[i].title}" class="grid-item" href="${projArray[i].link}">
+    let gridDiv = `<div id="${projArray[i].title}" class="grid-item">
+                    <a href="${projArray[i].link}">
                     <h2>${projArray[i].title}</h2>
                     <p>${projArray[i].info}</p>
                     <p>Start: ${projArray[i].startDate}</p>
                     <p>Update: ${projArray[i].lastUpdate}</p>
-                </div>`
+                    </a>
+                    </div>`
   gridDivs = gridDivs + gridDiv;
 }
 
