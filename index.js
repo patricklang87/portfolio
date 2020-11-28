7d946da3633c558cfaddb600e2361c4dee567581
Patrick <patricklang87@gmail.com>
2020-11-28 11:19:17 -0500

    add reynholm industries link

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ let pushNewInfo = (title, link, info, startDate, lastUpdate) => {
 	projArray.push(new ProjInfo(title, link, info, startDate, lastUpdate)); 
 }
 
+pushNewInfo("Reynholm Industries", "https://patricklang87.github.io/reynholm/", "A static welcome page for Reynholm Industries from the IT crowd designed to practice flexbox and grid in CSS. Uses screenshots from YouTube and images from the IT Crowd Fan Wiki.", "27 Nov 2020", "28 Nov 2020");
 
 pushNewInfo("Sentence Assesser", "tbd", "An adaptation of Levenstein Distance, meant to calculate the minimum number word order changes needed in order to match a given form. Part of a project to design an automatic sentence scoring program. Written in Python. Link not yet available.", "Oct 2020", "Oct 2020");
 
-pushNewInfo("Vocab List Bulider", "tbd", "A simple program that allows vocab words to be added to a database and then tested. Written in JavaScript to practice JSON, node.js, and database construction. Link not yet available.", "Sept 2020", "Sept 2020")
+pushNewInfo("Vocab List Builder", "tbd", "A simple program that allows vocab words to be added to a database and then tested. Written in JavaScript to practice JSON, node.js, and database construction. Link not yet available.", "Sept 2020", "Sept 2020")
 
 pushNewInfo("Battle Engine", "https://patricklang87.github.io/battle_engine/", "A simple RPG battle engine designed to practice the use of objects in JavaScript. Features Beverly, Battle Bot, and a Pack of Wild Dogs.", "Sept 2020", "Sept 2020");
 
