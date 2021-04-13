b77235ff16975c87dccf28a0de850d45b79dadd5
Patrick <patricklang87@gmail.com>
2021-04-13 12:53:18 -0400

    fix flag entry

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let pushNewInfo = (title, link, info, startDate, lastUpdate) => {
 	projArray.push(new ProjInfo(title, link, info, startDate, lastUpdate)); 
 }
 
-pushNewInfo("Learn Your Flags", "https://patricklang87.github.io/flagsFlashCars/", "Test and build your knowledge of country and territory flags. Practices React-Redux and React Routers using the restcountries.eu API.", "Feb 19 2021", "Feb 22 2021");
+pushNewInfo("Learn Your Flags", "https://patricklang87.github.io/flagsFlashCards/", "Test and build your knowledge of country and territory flags. Practices React-Redux and React Routers using the restcountries.eu API.", "April 5 2021", "April 13 2021");
 
 pushNewInfo("TicTacToe 3D", "https://patricklang87.github.io/tictactoe_3d/", "Another take on TicTacToe. Written to practice React and explore CSS's 3D capabilities.", "Feb 19 2021", "Feb 22 2021");
 
